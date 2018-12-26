3e18572715d7b0fb057857361e922d016bbe0ad5
wil pannell <wil.pannell@gmail.com>
2018-12-26 11:21:17 -0800

    unit testing in angular-video 22-heroes component: isolate component in test harness

--- src/app/hero/hero.component.spec.ts
+++ src/app/hero/hero.component.spec.ts
@@ -1,8 +1,22 @@
 import * as should from 'should';
+import {getTestBed, TestBed} from '@angular/core/testing';
+import {HeroesComponent} from '../heroes/heroes.component';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
 
 describe('hero component', () => {
   it(`has a passing canary test`, () => {
     true.should.be.true(`something's bugging the automated test infrastructure`);
+  });
+
+  afterEach(() => {
+    getTestBed().resetTestingModule();
+  });
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroesComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
   });
 });
 
