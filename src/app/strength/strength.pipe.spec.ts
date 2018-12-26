50fc47716e1117d2a4d74da7a9a29f12acc14845
wil pannell <wil.pannell@gmail.com>
2018-12-26 06:30:02 -0800

    unit testing in angular-video 13-strength pipe: make 1st test bar red

--- src/app/strength/strength.pipe.spec.ts
+++ src/app/strength/strength.pipe.spec.ts
@@ -1,5 +1,12 @@
+import * as should from 'should';
+import {StrengthPipe} from './strength.pipe';
+
 describe('strength pipe', () => {
-  it(`gives 'weak' for n < 10`);
+  it(`gives 'weak' for n < 10`, () => {
+    (new StrengthPipe()).transform(10000000).should.equal(
+        `-10000000 (weak)`);
+  });
+
   it(`gives 'strong' for 10 <= n < 20`);
   it(`gives 'unbelievable' for n >= 20`);
 });
