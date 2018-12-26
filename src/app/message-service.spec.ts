06a8bde8f00d98908fbe356e1ba2cad057cd8063
wil pannell <wil.pannell@gmail.com>
2018-12-26 07:02:34 -0800

    unit testing in angular-video 14-message service: make 3rd test bar refactor (rule of 3)

--- src/app/message-service.spec.ts
+++ src/app/message-service.spec.ts
@@ -2,23 +2,25 @@ import * as should from 'should';
 import {MessageService} from './message.service';
 
 describe('message service', () => {
+  let service;
+  beforeEach(() => {
+    service = new MessageService();
+  });
+
   it(`has a passing canary test`, () => {
     true.should.be.true(`something's bugging the automated test infrastructure`);
   });
 
   it(`starts with no messages`, () => {
-    const service = new MessageService();
     service.messages.length.should.equal(0);
   });
 
   it(`adds a message`, () => {
-    const service = new MessageService();
     service.add('dummy');
     service.messages.length.should.equal(1);
   });
 
   it(`clears all messages`, () => {
-    const service = new MessageService();
     service.add('dummy');
     service.clear();
     service.messages.length.should.equal(0);
