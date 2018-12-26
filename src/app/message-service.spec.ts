a89d339fe8bdf6309dfa67c9f0404a24a9e363b2
wil pannell <wil.pannell@gmail.com>
2018-12-26 06:54:48 -0800

    unit testing in angular-video 14-message service: make 1st test bar red

--- src/app/message-service.spec.ts
+++ src/app/message-service.spec.ts
@@ -1,11 +1,16 @@
 import * as should from 'should';
+import {MessageService} from './message.service';
 
 describe('message service', () => {
   it(`has a passing canary test`, () => {
     true.should.be.true(`something's bugging the automated test infrastructure`);
   });
 
-  it(`starts with no messages`);
+  it(`starts with no messages`, () => {
+    const service = new MessageService();
+    service.messages.length.should.equal(1);
+  });
+
   it(`adds a message`);
   it(`clears all messages`);
 });
