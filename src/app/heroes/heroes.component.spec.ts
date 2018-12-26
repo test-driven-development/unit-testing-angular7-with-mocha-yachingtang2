586fb3b91c24456f7d39ac5110831f4cd8c25bcb
wil pannell <wil.pannell@gmail.com>
2018-12-26 07:12:20 -0800

    unit testing in angular-video 15-heroes component: make delete collaboration spec fail

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,7 +1,9 @@
 import * as should from 'should';
+import {HeroesComponent} from './heroes.component';
+import {of} from 'rxjs/internal/observable/of';
 
 const td = require('testdouble');
-const {reset} = td;
+const {reset, when, constructor} = td;
 
 describe('heroes', () => {
   afterEach(() => {
@@ -12,7 +14,26 @@ describe('heroes', () => {
     true.should.be.true(`something's bugging the automated test infrastructure`);
   });
 
-  it(`collaborates with the heroes service to delete`);
+  it(`collaborates with the heroes service to delete`, () => {
+    // arrange
+    const hs = [{id: 1, name: 'a', strength: 8}, {id: 2, name: 'b', strength: 24}];
+
+    // require and create the heroService test double
+    const svc = new (constructor(require('../hero.service').HeroService))();
+
+    // stub deleteHero function to return an observable
+    when(svc.deleteHero(hs[0])).thenReturn(of());
+
+    // prepare the system under test
+    const component = new HeroesComponent(svc);
+    component.heroes = hs;
+    component.heroes.length.should.equal(2);
+
+    // act
+    component.delete(hs[0]);
+    // assert
+    component.heroes.length.should.equal(0);
+  });
 });
 
 
