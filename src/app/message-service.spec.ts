f5e45368f0fe13be65c70ea7aeba2923aaa27287
wil pannell <wil.pannell@gmail.com>
2018-12-26 06:51:40 -0800

    unit testing in angular-video 14-message service: design

--- src/app/message-service.spec.ts
+++ src/app/message-service.spec.ts
@@ -4,5 +4,8 @@ describe('message service', () => {
   it(`has a passing canary test`, () => {
     true.should.be.true(`something's bugging the automated test infrastructure`);
   });
-});
 
+  it(`starts with no messages`);
+  it(`adds a message`);
+  it(`clears all messages`);
+});
