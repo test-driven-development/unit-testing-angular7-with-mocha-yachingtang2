0930f513f64c426c1755bcef27b423711a17b1fd
AA Developer <developer@aa.com>
2019-01-03 10:11:58 -0600

    StrengthPipe design specs.

--- src/app/strength/strength.pipe.spec.ts
+++ src/app/strength/strength.pipe.spec.ts
@@ -2,4 +2,7 @@ describe('StrengthPipe', () => {
   it('canary test should be successful', () => {
     true.should.be.true();
   });
+  it('show strength 1 is weak');
+  it('show strength 11 is strong');
+  it('show strength 21 is unbelievable');
 });
