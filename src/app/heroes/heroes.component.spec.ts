deb4d3aec1f8e71e19e42770ff63ba2628d622a7
AA Developer <developer@aa.com>
2019-01-03 11:21:57 -0600

    HeroesComponent - design spec.

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,5 @@
+describe('HeroesComponent', () => {
+  it('return heroes when getHeroes() is called');
+  it('add a hero to existing heroes when add() is called');
+  it('delete a hero when delete is called');
+});
