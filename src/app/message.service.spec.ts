456121a5dae6ceb6719da71dec1102d5bc729099
AA Developer <developer@aa.com>
2019-01-03 10:49:49 -0600

    MessageService - add a message, length is 1.

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -4,6 +4,13 @@ describe('MessageService', () => {
   it('no message, the length is 0', () => {
     new MessageService().messages.length.should.equal(0);
   });
-  it('add a message, the length is 1');
+
+  it('add a message, the length is 1', () => {
+    const messageService = new MessageService();
+    messageService.add('testing');
+
+    messageService.messages.length.should.equal(1);
+  });
+
   it('clear message, the length is 0');
 });
