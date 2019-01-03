91056bc4f9ee9430797f0a39e3c6c62266691a41
AA Developer <developer@aa.com>
2019-01-03 10:47:56 -0600

    MessageService - no message length is 0.

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -1,5 +1,9 @@
+import {MessageService} from './message.service';
+
 describe('MessageService', () => {
-  it('no message, the length is 0');
+  it('no message, the length is 0', () => {
+    new MessageService().messages.length.should.equal(0);
+  });
   it('add a message, the length is 1');
   it('clear message, the length is 0');
 });
