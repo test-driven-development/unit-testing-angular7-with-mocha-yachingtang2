dfeee7dd38dfd745e3566fb59fc8a71c809199ad
AA Developer <developer@aa.com>
2019-01-03 14:44:40 -0600

    HeroesComponent - add hero scenario.

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -20,6 +20,21 @@ describe('HeroesComponent', () => {
     });
   });
 
-  it('add a hero to existing heroes when add() is called');
+  it.only('add a hero to existing heroes when add() is called', () => {
+    const heroes = [{id: 1, name: 'a', strength: 8}, {id: 2, name: 'b', strength: 24}];
+    const heroService = new (constructor(require('../hero.service').HeroService))();
+    const hero = {id: 3, name: 'c', strength: 11};
+
+    when(heroService.addHero({name: 'c', strength: 11})).thenReturn(of(hero));
+
+    const heroesComponent = new HeroesComponent(heroService);
+    heroesComponent.heroes = heroes;
+
+    heroesComponent.add('c').then(() => {
+      heroesComponent.heroes.length.should.equal(3);
+      heroesComponent.heroes[2].should.deepEqual(hero);
+    });
+  });
+
   it('delete a hero when delete is called');
 });
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -23,25 +23,18 @@ export class HeroesComponent implements OnInit {
     });
   }
 
-  add(name: string): void {
+  add(name: string): Promise<void> {
     name = name.trim();
     const strength = 11;
     if (!name) { return; }
-    this.heroService.addHero({ name, strength } as Hero)
-      .subscribe(hero => {
-        this.heroes.push(hero);
-      });
+
+    return this.heroService.addHero({ name, strength } as Hero).toPromise().then(hero => {
+      this.heroes.push(hero);
+    });
   }
 
   delete(hero: Hero): void {
     this.heroes = this.heroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero).subscribe();
   }
-
-  // delete(hero: Hero): Promise<any> {
-  //   return this.heroService.deleteHero(hero).toPromise().then(() => {
-  //     this.heroes = this.heroes.filter(h => h !== hero);
-  //     // return hero;
-  //   });
-  // }
 }
